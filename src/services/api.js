9a4de22cc737e912a09062efaa7f8409ebc4e209
Siigmaf <faby.tbe@gmail.com>
2022-09-08 17:12:16 -0300

    conclui requisito 01

--- src/services/api.js
+++ src/services/api.js
@@ -1,12 +1,30 @@
 export async function getCategories() {
-  // Implemente aqui teste requisito 1
+  // https://api.mercadolibre.com/sites/MLB/categories
+  // https://api.mercadolibre.com/sites/MLB/search?category=$CATEGORY_ID&q=$QUERY
+  const api1 = ('https://api.mercadolibre.com/sites/MLB/categories');
+  try {
+    const endpoint = await fetch(api1);
+    const resultado = await endpoint.json();
+    return await resultado;
+  } catch (error) {
+    throw new Error('You must provide an url');
+  }
 }
-
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+console.log('string');
+getCategories();
+// Implemente aqui teste requisito 1
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  const apiIdQuery = (`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`);
+  try {
+    const endpoint = await fetch(apiIdQuery);
+    const resultado = await endpoint.json();
+    return resultado;
+  } catch (error) {
+    throw new Error('You must provide an url');
+  }
 }
-
+// Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
 export async function getProductById() {
-  // Esta implementação específica não é avaliada, mas pode ajudar você 🙂
+  // Esta implementação específica não é avaliada, mas pode ajudar você :slightly_smiling_face:
   // Atenção: essa função não deverá ser chamada na tela do carrinho de compras.
 }
