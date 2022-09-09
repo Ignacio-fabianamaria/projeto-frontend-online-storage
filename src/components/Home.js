887d15838ff78aa77142e0c3b3b9094de12c335d
BenHur Albertassi <benhuralber@yahoo.com.br>
2022-09-09 15:51:54 -0300

    requisito 5

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,11 +3,24 @@ import { Link } from 'react-router-dom';
 import { getCategories } from '../services/api';
 
 class Home extends React.Component {
-  state = {
-    data: [],
-    // name: '',
-    // price:'',
-  };
+  constructor() {
+    super();
+
+    this.trocarInput = this.trocarInput.bind(this);
+    this.botaoParaLocalizar = this.botaoParaLocalizar.bind(this);
+
+    state = {
+      busca: '',
+      data: [],
+      infoFrase: true,
+      mensagem: '',
+      name: '',
+      nomeProduto: '',
+      produtoImagem: '',
+      precoProduto: 0,
+      arrayLista: [],
+    };
+  }
 
   async componentDidMount() {
     this.functionGetCategories();
@@ -19,8 +32,23 @@ class Home extends React.Component {
     this.setState({ data: response });
   };
 
+  trocarInput(evento) {
+    this.setState({ name: evento.target.value }, () => {
+    });
+  }
+
+  async botaoParaLocalizar() {
+    const { name } = this.state;
+    this.setState({ name: '' });
+    const resultado = await getProductsFromCategoryAndQuery(name);
+    this.setState = {
+      nomeProduto: name,
+      arrayLista: resultado,
+    };
+  }
+
   render() {
-    const { data } = this.state;
+    const { data, name, arrayLista } = this.state;
     return (
       <div>
         <form>
@@ -29,11 +57,14 @@ class Home extends React.Component {
             <input
               type="text"
               id="search-input"
+              value={ name }
+              onChange={ this.trocarInput }
             />
           </label>
           <div htmlFor="query-button">
             <button
-              type="submit"
+              onClick={ this.botaoParaLocalizar }
+              type="button"
               data-testid="query-button"
             >
               Search
@@ -53,19 +84,25 @@ class Home extends React.Component {
           </Link>
         </div>
         <div>
-          {data.map((element) => (
-            <label
-              htmlFor={ element.id }
-              key={ element.id }
-              data-testid="category"
-            >
-              <input
-                type="checkbox"
-                name={ element.name }
-                id={ element.id }
-              />
-              {element.name}
-            </label>
+          {arrayLista.map((element) => (
+            <div key={ element.id }>
+              <label
+                htmlFor={ element.id }
+                data-testid="category"
+              >
+                <input
+                  type="checkbox"
+                  name={ element.title }
+                  id={ element.id }
+                />
+
+              </label>
+              <p>
+                {element.title}
+              </p>
+              <image src={ element.thumbnail } />
+
+            </div>
           ))}
         </div>
       </div>
